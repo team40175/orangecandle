c7cea5a9814809181957c9a914f5b3c649e1c116
gambiti <gambitid9@gmail.com>
2015-04-24 19:33:24 +0300

    .

---
+++ orangecandle-server/src/main/java/com/orangecandle/repository/Building.java
@@ -0,0 +1,7 @@
+package com.orangecandle.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Building extends
+	JpaRepository<com.orangecandle.domain.Building, String> {
+}
