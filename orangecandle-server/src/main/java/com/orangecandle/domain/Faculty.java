38cdc2340b46e3a8065d1d7fb2e7ac7c82b136ec
gambiti <gambitid9@gmail.com>
2015-04-24 19:01:21 +0300

    .

--- orangecandle-server/src/main/java/com/orangecandle/domain/Faculty.java
+++ orangecandle-server/src/main/java/com/orangecandle/domain/Faculty.java
@@ -1,5 +1,6 @@
 package com.orangecandle.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -11,8 +12,27 @@ import javax.persistence.Table;
 @Entity
 @Table(name="faculty")
 public class Faculty {
-	private @Id @Column(name = "id") String id;
+	private @Id @Column(name = "id") Integer id;
 	private @Column(name = "facultyname") String name;
 	private @OneToMany List<Department> departments;
 	
+	public Integer getId(){
+		return this.id;
+	}
+	public void setId(Integer id){
+		this.id=id;
+	}
+	public String getDepartmentName(){
+		return this.name;
+	}
+	public void setDepartmentName(String name){
+		this.name=name;
+	}
+	public void addDepartment(Department dep){
+		if(departments.isEmpty()) departments=new ArrayList<Department>();
+		departments.add(dep);
+	}
+	public List<Department> getDepartments(){
+		return departments;
+	}
 }
--- orangecandle-server/src/main/java/com/orangecandle/domain/School.java
+++ orangecandle-server/src/main/java/com/orangecandle/domain/School.java
@@ -1,5 +1,30 @@
 package com.orangecandle.domain;
 
-public class School {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="school")
+public class School {
+	private @Column(name="schoolname") String schoolName;
+	private @OneToMany List<Faculty> faculties;
+	
+	public String getName() {
+		return schoolName;
+	}
+	public void setName(String name) {
+		this.schoolName=name;
+	}
+	public void addFaculty(Faculty fac) {
+		if(faculties.isEmpty()) faculties=new ArrayList<Faculty>();
+		this.faculties.add(fac);
+	}
+	public List<Faculty> listFaculty() {
+		return this.faculties;
+	}
 }
