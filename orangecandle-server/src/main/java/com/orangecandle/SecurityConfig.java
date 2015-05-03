f87c6d12ac814508666afadbe1dad44acbbc52f1
can <bunyacloven@gmail.com>
2015-05-04 02:40:43 +0300

    WebFilter added for CORS compatibility

--- orangecandle-server/src/main/java/com/orangecandle/SecurityConfig.java
+++ orangecandle-server/src/main/java/com/orangecandle/SecurityConfig.java
@@ -4,16 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+import com.orangecandle.filter.CorsFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth)
-			throws Exception {
-		System.out.println("______________");
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("a").password("a")
 				.authorities("USER");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
+				.authorizeRequests().anyRequest().authenticated().and()
+				.formLogin();
 	}
 }
