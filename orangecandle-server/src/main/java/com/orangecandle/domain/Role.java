cb309fc4e40fa3862e6c5fc22ec788ee5bcd5081
can <bunyacloven@gmail.com>
2015-05-15 15:44:17 +0300

    role now implements grantedauthority

--- orangecandle-server/src/main/java/com/orangecandle/domain/Role.java
+++ orangecandle-server/src/main/java/com/orangecandle/domain/Role.java
@@ -1,5 +1,12 @@
 package com.orangecandle.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
 	Administrator, Advisor, Lecturer, Student;
+
+	@Override
+	public String getAuthority() {
+		return name();
+	}
 }
