905c3e44dcc70b2edf12f6ca8b99eec82951981a
can <bunyacloven@gmail.com>
2015-05-15 21:27:52 +0300

    lmgtfy

--- orangecandle-client/app/util/Auth.js
+++ orangecandle-client/app/util/Auth.js
@@ -1,9 +1,13 @@
 Ext.define('OrangeCandle.util.Auth', {
 	singleton : true,
 	set : function(username, password) {
+		this.username = username;
 		this.auth = 'Basic ' + btoa(username + ':' + password);
 	},
 	get : function() {
 		return this.auth;
+	},
+	getUsername : function() {
+		return this.username;
 	}
 });
