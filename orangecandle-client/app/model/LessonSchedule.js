c8ebb6584e50b2bb3ca8494369319f4b86dac8e7
SwaghettiYolognese <SwaghettiYolognese@users.noreply.github.com>
2015-05-22 21:36:53 +0300

    schedule view added

--- orangecandle-client/app/model/LessonSchedule.js
+++ orangecandle-client/app/model/LessonSchedule.js
@@ -1,21 +1,6 @@
 Ext.define('OrangeCandle.model.LessonSchedule', {
 	extend : 'Ext.data.Model',
 	config : {
-		fields : [ {
-			name : 'Monday',
-			type : 'auto'
-		}, {
-			name : 'Tuesday',
-			type : 'auto'
-		}, {
-			name : 'Wednesday',
-			type : 'auto'
-		}, {
-			name : 'Thursday',
-			type : 'auto'
-		},{
-			name : 'Friday',
-			type : 'auto'
-		}  ]
+		fields : ['name']
 	}
 });
--- orangecandle-client/app/view/ViewLessons.js
+++ orangecandle-client/app/view/ViewLessons.js
@@ -1,38 +1,17 @@
 Ext.define('OrangeCandle.view.ViewLessons', {
 	extend : 'Ext.form.Panel',
 	alias : 'widget.viewlessons',
-	requires : [ 'Ext.form.Panel', 'Ext.form.FieldSet' ],
+	requires : [ 'Ext.form.Panel', 'Ext.form.FieldSet',
+			'Ext.dataview.NestedList', 'Ext.data.TreeStore' ],
 	config : {
-		layout : 'card',
 		scrollable : 'false',
+		layout : 'fit',
 		items : [ {
-			xtype : 'grid',
-			name : 'lessonlist',
-			itemId : 'grouplist',
-			itemTpl : '{name}',
-			store : 'Role',
-			columns : [ {
-				text : 'Monday',
-				dataIndex : 'Monday',
-				width : 150
-			}, {
-				text : 'Tuesday',
-				dataIndex : 'Tuesday',
-				width : 150
-			}, {
-				text : 'Wednesday',
-				dataIndex : 'Wednesday',
-				width : 150
-			}, {
-				text : 'Thursday',
-				dataIndex : 'Thursday',
-				width : 150
-			},{
-				text : 'Friday',
-				dataIndex : 'Friday',
-				width : 150
-			}  ]
-
+			title : 'Weekly Schedule',
+			itemId : 'schedule',
+			xtype : 'nestedlist',
+			store : 'LessonSchedule',
+			displayField:'name'
 		} ]
 	}
 })
